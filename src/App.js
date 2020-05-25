45c9658c3e3c37ffbe674cd0a2a4b073ddeeea61
David Swiatkiewiez <davidswiatkiewiez@MacBook-Pro-de-David-Swiatkiewiez.local>
2020-05-26 00:57:23 +0200

    test react-three-fiber et react-spring

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, useState, useRef } from 'react';
+import * as THREE from "three";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { Canvas, extend, useThree, useFrame } from 'react-three-fiber';
+import { useSpring, a, useSprings } from 'react-spring/three';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+extend({ OrbitControls });
+
+const Controls = () => {
+  const orbitRef = useRef();
+  const { camera, gl } = useThree();
+
+  useFrame(() => {
+    orbitRef.current.update()
+  }); 
+
+  return(
+    <orbitControls 
+    args = {[ camera, gl.domElement ]}
+    ref={orbitRef}
+    />
+  )
+}
+
+const Box = () => {
+   const [Hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
+  const props = useSpring({
+    scale : active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+    color : Hovered ? "hotpink" : "gray"
+  })
+
+  return(
+  <a.mesh
+    onPointerOver={() => setHovered(true)}
+    onPointerOut={() => setHovered(false)}
+    onClick={() => setActive(!active)}
+    scale={props.scale}
+  >
+    <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+    <a.meshBasicMaterial attach="material" color={props.color} />
+  </a.mesh>
+  )
+}
+
+class App extends Component {
+
+  render() {
+    return (
+      <Canvas>
+        <Controls />
+        <Box />
+      </Canvas>
+    );
+  }
 }
 
 export default App;
